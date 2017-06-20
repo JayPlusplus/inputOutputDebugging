26c9a96236040688e75caa7a4c1d6c0e630c4e0b
Jay Wu <wu274319426@gmail.com>
2017-06-20 10:44:19 -0400

    doubleAverage

--- src/inputOutput/InputOutputDebugging.java
+++ src/inputOutput/InputOutputDebugging.java
@@ -6,7 +6,7 @@ public class InputOutputDebugging {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	    Scanner keyboard = new Scanner(System.in);
+	   /* Scanner keyboard = new Scanner(System.in);
 
 	   int num1, num2, num3;
 
@@ -19,7 +19,22 @@ public class InputOutputDebugging {
 	    System.out.print( "Third integer? " );
 	    num3 = keyboard.nextInt();
 	  
-	  System.out.println("The total is : " + (num1 + num2 + num3));
-	}
+	  System.out.println("The total is : " + (num1 + num2 + num3));*/
+		
+		
+		
+		double num1, num2, num3;
+		Scanner keyboard = new Scanner(System.in);
+		
+	    System.out.print( "First temperature? " );
+	    num1 = keyboard.nextDouble();
+
+	    System.out.print( "Second temperature? " );
+	    num2 = keyboard.nextDouble();
+
+	    System.out.println("The average value is : " + (num1 + num2)/2);
+	  }
+	
+	
 
 }
